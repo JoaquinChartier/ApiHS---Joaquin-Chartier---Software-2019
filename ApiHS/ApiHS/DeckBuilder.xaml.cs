c08782c4010c90bb3ff87a49f41baa305642910c
JoaquinChartier <48659409+JoaquinChartier@users.noreply.github.com>
2019-11-15 16:59:01 -0300

    Sistema de eliminado de cartas.

--- ApiHS/ApiHS/DeckBuilder.xaml.cs
+++ ApiHS/ApiHS/DeckBuilder.xaml.cs
@@ -31,18 +31,18 @@ namespace ApiHS
         public int CantidadCartasMazo;
 
 
+
         public DeckBuilder()
         {
             InitializeComponent();
             TraerToken();
-            MazoPorAgregar.Cartas = new Dictionary<int, int>();
+            MazoPorAgregar.Cartas = new Dictionary<int, int>(); //Inicializo el diccionario
         }
 
         private void TraerToken()
         {
             var client = new RestClient("https://us.battle.net"); //le paso la url para hacer el request
             client.Authenticator = new HttpBasicAuthenticator(DevUser, DevSecret);//le paso los datos de auth(user y pass)
-
             var request = new RestRequest("/oauth/token", Method.POST); //le digo el resource y el metodo
             request.AddParameter("grant_type", "client_credentials"); // ingreso los parametros para hacer request
             var response = client.Execute<SolicitudHTTP>(request); // ejecuta el request y lo deserializa automaticamente
@@ -64,63 +64,112 @@ namespace ApiHS
             IndexSeleccionado = e.SelectedItemIndex; //Guardo en variable el index del elemento seleccionado
             VisorCartas.Source = ResultList[IndexSeleccionado].image; //Asigno al visor la imagen correspondiente al index
 
-            //IF anidado que se ejecuta al seleccionar un index
-            if (MazoPorAgregar.Cartas.ContainsKey(ResultList[IndexSeleccionado].id))//Chequea si la carta ya fue agregada
+            var esLegendaria = ResultList[IndexSeleccionado].rarityId == 5;
+            var fueAgregada = MazoPorAgregar.Cartas.ContainsKey(ResultList[IndexSeleccionado].id);
+
+            if (fueAgregada)//Chequea si la carta ya fue agregada
             {
-                if ((MazoPorAgregar.Cartas[ResultList[IndexSeleccionado].id] == 2) ||//Chequea si no hay dos cartas iguales ya ingresadas
-                    ((ResultList[IndexSeleccionado].rarityId == 5) && (MazoPorAgregar.Cartas[ResultList[IndexSeleccionado].id] == 1)))//O si la carta es una legendaria y solo hay una,(solo se permite una leg. por mazo)
-                {
-                    Agregar.SetValue(IsEnabledProperty, false);//si se cumplio alguna de las dos opciones desactiva el botón
-                    //si es una rara setea el contador en 1/1, sino en 2/2
-                    if (ResultList[IndexSeleccionado].rarityId == 5) { LabelContador.Text = "1/1"; }
-                    else { LabelContador.Text = "2/2"; }
-                }
-                else { LabelContador.Text = "1/2"; }//Si la carta ya fue agregada, pero solo una vez setea en 1/2
+                Borrar.IsVisible = true;
+                SetterLabelContador();
             }
             else
-            {//Si no fue agregada, el botón se mantiene encendido
-                if (CantidadCartasMazo < 30) { Agregar.SetValue(IsEnabledProperty, true); } 
-                //si es una rara setea el contador en 0/1, sino en 0/2
-                if (ResultList[IndexSeleccionado].rarityId == 5) { LabelContador.Text = "0/1"; }
-                else { LabelContador.Text = "0/2"; }
+            {   
+                Borrar.IsVisible = false;
+                if (CantidadCartasMazo < 30) {Agregar.SetValue(IsEnabledProperty, true);}//Si no fue agregada, el botón se enciende
+                SetterLabelContador();
             }
         }
 
         private void AgregarCliqueado(object sender, EventArgs e)
-        {//Este boton agrega la carta al mazo por construir
-            if (MazoPorAgregar.Cartas.ContainsKey(ResultList[IndexSeleccionado].id) && (!(ResultList[IndexSeleccionado].rarityId == 5)))//Chequea si existe la key en el diccionario y no es una legendaria
+        {
+            var esLegendaria = ResultList[IndexSeleccionado].rarityId == 5;
+            var fueAgregada = MazoPorAgregar.Cartas.ContainsKey(ResultList[IndexSeleccionado].id);
+                              
+            if (fueAgregada && (!esLegendaria))//Chequea si existe la key en el diccionario y no es una legendaria
             {
                 MazoPorAgregar.Cartas[ResultList[IndexSeleccionado].id] = 2;//Si ya existe agrega una mas
-                //si es una legendaria setea el contador en 1/1, sino en 2/2
-                if (ResultList[IndexSeleccionado].rarityId == 5) { LabelContador.Text = "1/1"; }
-                else { LabelContador.Text = "2/2"; }
+                SetterLabelContador();
+                Agregar.SetValue(IsEnabledProperty, false);
             }
             else
             { //si es una legendaria...
-                if (ResultList[IndexSeleccionado].rarityId == 5) {
-                    if (MazoPorAgregar.Cartas.ContainsKey(ResultList[IndexSeleccionado].id))
-                    { Agregar.SetValue(IsEnabledProperty, false); }//... y ya existe desactiva el boton
+                if (esLegendaria) {
+                    if (fueAgregada)
+                    { Agregar.SetValue(IsEnabledProperty, false); } //... y ya existe desactiva el boton
                     else { 
-                    LabelContador.Text = "1/1";//si es legendaria pero no existe agrega una y desactiva el boton
-                    MazoPorAgregar.Cartas.Add(ResultList[IndexSeleccionado].id, 1); 
+                    //si es legendaria pero no existe agrega una y desactiva el boton
+                    MazoPorAgregar.Cartas.Add(ResultList[IndexSeleccionado].id, 1);
+                    SetterLabelContador();
                     Agregar.SetValue(IsEnabledProperty, false);
+                    Borrar.IsVisible = true;
                     }
                 }
                 else
                 {//si no es legendaria pero tampoco existe agrega una al diccionario
-                    LabelContador.Text = "1/2";
                     MazoPorAgregar.Cartas.Add(ResultList[IndexSeleccionado].id, 1);
+                    SetterLabelContador();
+                    Borrar.IsVisible = true;
                 }
-                //Realizar aca prueba de escritorio
             }
-            ChequeoCantidadCartasAgregadas();
+            SetterCantidadCartasAgregadas();
         }
 
-        private void ChequeoCantidadCartasAgregadas()
+        private void SetterCantidadCartasAgregadas()
         {
             CantidadCartasMazo = MazoPorAgregar.Cartas.Values.Sum(); //Suma todos los values presentes en el diccionario
             TotalCartasMazo.Text = CantidadCartasMazo+"/30"; //Asigna el valor al label
             if (CantidadCartasMazo==30) { Agregar.SetValue(IsEnabledProperty, false); } //Si la cantidad llegó a 30, desactiva el botón
+        }
+
+        private void BorrarCliqueado(object sender, EventArgs e)
+        {
+            var esLegendaria = ResultList[IndexSeleccionado].rarityId == 5;
+            if (esLegendaria)
+            {
+                MazoPorAgregar.Cartas.Remove(ResultList[IndexSeleccionado].id);
+                Borrar.IsVisible = false;
+                SetterCantidadCartasAgregadas();
+                SetterLabelContador();
+                Agregar.SetValue(IsEnabledProperty, true);
+            }
+            else
+            {
+                if (MazoPorAgregar.Cartas[ResultList[IndexSeleccionado].id] == 2)
+                {
+                    MazoPorAgregar.Cartas[ResultList[IndexSeleccionado].id] = 1;
+                    SetterCantidadCartasAgregadas();
+                    SetterLabelContador();
+                }
+                else
+                {
+                    MazoPorAgregar.Cartas.Remove(ResultList[IndexSeleccionado].id);
+                    Borrar.IsVisible = false;
+                    SetterCantidadCartasAgregadas();
+                    SetterLabelContador();
+                }
+                Agregar.SetValue(IsEnabledProperty, true);
+            }
+        }
+
+        private void SetterLabelContador()
+        {
+            var esLegendaria = ResultList[IndexSeleccionado].rarityId == 5;
+            var fueAgregada = MazoPorAgregar.Cartas.ContainsKey(ResultList[IndexSeleccionado].id);
+
+            if (fueAgregada)
+            {
+                var valueActual = MazoPorAgregar.Cartas[ResultList[IndexSeleccionado].id].ToString();
+                if (esLegendaria) { LabelContador.Text = "1/1"; }
+                else {
+                    LabelContador.Text = valueActual + "/2";
+                    if (valueActual == "1") { Agregar.SetValue(IsEnabledProperty, true); }
+                     }
+            }
+            else
+            {
+                if (esLegendaria) { LabelContador.Text = "0/1"; }
+                else { LabelContador.Text = "0/2"; }
+            }
         }
     }
 }
